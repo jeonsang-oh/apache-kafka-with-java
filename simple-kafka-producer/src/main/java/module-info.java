eb79fd2b404a71d24c0835d571502553afa619af
jeonsang-oh <jeon.sangoh92@gmail.com>
2023-01-17 20:43:31 +0900

    kafkaStreams

--- simple-kafka-producer/src/main/java/module-info.java
+++ simple-kafka-producer/src/main/java/module-info.java
@@ -2,18 +2,15 @@ module com.example.simplekafkaproducer {
 
     requires org.slf4j;
     requires kafka.clients;
+    requires kafka.streams;
 
 
-    opens com.example.simplekafkaproducer to javafx.fxml;
-    exports com.example.simplekafkaproducer;
-    exports com.example.chapter3.consumer;
-    opens com.example.chapter3.consumer to javafx.fxml;
-    exports com.example.chapter3.producer;
-    opens com.example.chapter3.producer to javafx.fxml;
+    opens com.example.chapter3.producer.simplekafkaproducer to javafx.fxml;
+    exports com.example.chapter3.producer.simplekafkaproducer;
+    exports com.example.chapter3.consumer.simpleConsumer;
     exports com.example.chapter3.producer.kafkaProducerAsyncCallback;
     opens com.example.chapter3.producer.kafkaProducerAsyncCallback to javafx.fxml;
     exports com.example.chapter3.producer.kafkaProducerCustomPartitioner;
     opens com.example.chapter3.producer.kafkaProducerCustomPartitioner to javafx.fxml;
-    exports com.example.chapter3.consumer.simpleConsumer;
     opens com.example.chapter3.consumer.simpleConsumer to javafx.fxml;
 }
